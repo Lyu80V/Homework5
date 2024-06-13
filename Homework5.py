2e7d1a05c29168a9cf813c4dbf7ecd70d99f0bef
Lyu80V <7222707@gmail.com>
2024-06-13 19:07:34 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,8 @@
+immutable_var=1,'a',2,'b',3,'c'
+print('Immutable var:',immutable_var)
+mutable_list=[1,'a',2,'b',3,'c','alphabet']
+print('Mutable list:',mutable_list)
+mutable_list[3]="B"
+print('Mutable list:',mutable_list)
+mutable_list.append(4)
+print('Mutable list:',mutable_list)
